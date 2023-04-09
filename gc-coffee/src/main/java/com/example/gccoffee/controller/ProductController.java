b7aa05fc504fff7cb4233b63f48b7a15e2289aa8
woody35545 <woody35545@gmail.com>
2023-04-09 17:42:57 +0900

    [+] Add newProduct(create) Post mapping

--- gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -25,4 +26,13 @@ public class ProductController {
         return "new-product";
     }
 
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(
+                createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
+    }
 }
